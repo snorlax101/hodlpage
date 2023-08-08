30707a23cfe472c64c64b8f91058dff6cbc1a446
Vaishali Barua <vaishalibaruaofficial@gmail.com>
2023-08-08 16:42:51 +0530

    Added frontend and backend.

---
+++ app.js
@@ -0,0 +1,110 @@
+// app.js
+
+const express = require('express');
+const mongoose = require('mongoose');
+const https = require('https');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
+const mongoURI = 'mongodb://127.0.0.1:27017/cryptocurrencies'; // Replace with your MongoDB connection URL
+
+// Connect to MongoDB
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// Define a schema for storing cryptocurrency data
+const cryptoSchema = new mongoose.Schema({
+  name: String,
+  last: Number,
+  buy: Number,
+  sell: Number,
+  volume: Number
+});
+
+const Crypto = mongoose.model('Crypto', cryptoSchema);
+
+async function clearExistingData() {
+    try {
+      await Crypto.deleteMany({});
+      console.log('Existing data cleared.');
+    } catch (error) {
+      console.error('Error clearing existing data:', error);
+    }
+  }
+
+// Fetch data from WazirX API and store in MongoDB
+async function fetchDataAndStore() {
+  await clearExistingData();
+  try {
+    https.get('https://api.wazirx.com/api/v2/tickers', (response) => {
+      let data = '';
+
+      response.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      response.on('end', async () => {
+        const tickers = JSON.parse(data);
+
+        // Extract top 10 tickers
+        const top10Tickers = Object.values(tickers).slice(0, 10);
+        //console.log(JSON.stringify(top10Tickers));
+
+        // Store data in MongoDB
+        for (const ticker of top10Tickers) {
+          const { base_unit, last, buy, sell, volume } = ticker;
+          const crypto = new Crypto({
+            name: base_unit.toUpperCase(),
+            last,
+            buy,
+            sell,
+            volume
+          });
+          console.log('Saving data:', crypto);
+          try {
+            await crypto.save();
+            console.log('Data saved:', crypto);
+          } 
+          catch (saveError) {
+          console.error('Error saving data:', saveError);
+          }
+        }
+
+        console.log('Data stored in MongoDB.');
+      });
+    });
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+
+// Call the fetchDataAndStore function to fetch and store data at the start of the server
+fetchDataAndStore();
+
+app.get('/api/cryptos', async (req, res) => {
+    try {
+      const cryptos = await Crypto.find({}, '-_id name last buy sell volume').limit(10);
+      res.json(cryptos);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      res.status(500).json({ error: 'Error fetching data' });
+    }
+  });
+
+const PORT = 3000; 
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ public/main.js
@@ -0,0 +1,34 @@
+// main.js
+
+document.addEventListener('DOMContentLoaded', () => {
+  const tableBody = document.querySelector('tbody');
+
+  // Fetch cryptocurrency data from the backend API
+  fetch('http://localhost:3000/api/cryptos')
+    .then((response) => response.json())
+    .then((data) => {
+      // Populate the table with the retrieved data
+      data.forEach((crypto) => {
+        const { name, last, buy, sell, volume, base_unit } = crypto;
+
+        const priceDifferencePercentage = ((sell - buy) / buy) * 100;
+        const colorClass = priceDifferencePercentage >= 0 ? 'green' : 'red';
+
+        const savings = sell - buy;
+        const savingsIcon = savings >= 0 ? '▲' : '▼';
+
+        const row = document.createElement('tr');
+        row.innerHTML = `
+          <td>${name}</td>
+          <td>₹${last.toFixed(2)}</td>
+          <td>₹${buy.toFixed(2)} / ₹${sell.toFixed(2)}</td>
+          <td class="${colorClass}">${priceDifferencePercentage.toFixed(2)}%</td>
+          <td class="${colorClass}">${savingsIcon} ₹${Math.abs(savings).toFixed(2)}</td>
+        `;
+        tableBody.appendChild(row);
+      });
+    })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+    });
+});
